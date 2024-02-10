1a33ff3db9e9310746f79fb50900c5030922caf1
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-02-10 18:03:18 +0100

    Excercise 3.8

--- src/test/kotlin/chapter3/exercises/ex5/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex5/listing.kt
@@ -1,32 +1,37 @@
 package chapter3.exercises.ex5
 
+import chapter3.Cons
 import chapter3.List
 import chapter3.Nil
+import chapter4.size
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
 // tag::init[]
-fun <A> init(l: List<A>): List<A> =
+tailrec fun <A> init(l: List<A>): List<A> {
+    return when(l) {
+        is Nil -> throw IllegalStateException()
+        is Cons -> if(l.size() == 1) Nil else Cons(l.head,init(l.tail))
+    }
+}
 
-    SOLUTION_HERE()
 // end::init[]
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise5 : WordSpec({
 
     "list init" should {
-        "!return all but the last element" {
+        "return all but the last element" {
             init(List.of(1, 2, 3, 4, 5)) shouldBe
                 List.of(1, 2, 3, 4)
         }
 
-        "!return Nil if only one element exists" {
+        "return Nil if only one element exists" {
             init(List.of(1)) shouldBe Nil
         }
 
-        "!throw an exception if no elements exist" {
+        "throw an exception if no elements exist" {
             shouldThrow<IllegalStateException> {
                 init(List.empty<Int>())
             }
