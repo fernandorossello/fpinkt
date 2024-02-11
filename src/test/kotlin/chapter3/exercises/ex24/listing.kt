7b35d5bb596a423d6aa698c35875c7c4277345d0
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-02-11 20:59:47 +0100

    Excercise 3.24

--- src/test/kotlin/chapter3/exercises/ex24/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex24/listing.kt
@@ -9,14 +9,15 @@ import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A> size(tree: Tree<A>): Int =
-
-    SOLUTION_HERE()
+    when(tree) {
+        is Leaf -> 1
+        is Branch -> size(tree.left) + size(tree.right) + 1
+    }
 // end::init[]
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise24 : WordSpec({
     "tree size" should {
-        "!determine the total size of a tree" {
+        "determine the total size of a tree" {
             val tree =
                 Branch(
                     Branch(Leaf(1), Leaf(2)),
