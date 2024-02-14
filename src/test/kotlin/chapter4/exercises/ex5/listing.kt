99b530dc4b693dac1ece82a79da479c46458f8e6
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-02-14 20:08:49 +0100

    Excercise 4.5

--- src/test/kotlin/chapter4/exercises/ex5/listing.kt
+++ src/test/kotlin/chapter4/exercises/ex5/listing.kt
@@ -1,28 +1,33 @@
 package chapter4.exercises.ex5
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.Nil
 import chapter4.None
 import chapter4.Option
 import chapter4.Some
+import chapter4.exercises.ex1.orElse
+import chapter4.foldRight
+import chapter4.getOrElse
+import chapter4.map2
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise5 : WordSpec({
 
     //tag::traverse[]
     fun <A, B> traverse(
         xa: List<A>,
-        f: (A) -> Option<B>
+        f: (A) -> Option<B> // toString o toInt
     ): Option<List<B>> =
-
-        SOLUTION_HERE()
+        xa.foldRight(Some(Nil)) { ha: A, ta:Option<List<B>> -> map2(f(ha),ta)
+            { ob:B, olb:List<B> -> Cons(ob,olb) } }
     //end::traverse[]
 
     fun <A> sequence(xs: List<Option<A>>): Option<List<A>> =
+        traverse(xs) { it }
 
-        SOLUTION_HERE()
 
     fun <A> catches(a: () -> A): Option<A> =
         try {
@@ -32,7 +37,7 @@ class Exercise5 : WordSpec({
         }
 
     "traverse" should {
-        """!return some option of a transformed list if all
+        """return some option of a transformed list if all
             transformations succeed""" {
             val xa = List.of(1, 2, 3, 4, 5)
             traverse(xa) { a: Int ->
@@ -42,7 +47,7 @@ class Exercise5 : WordSpec({
             )
         }
 
-        "!return a none option if any transformations fail" {
+        "return a none option if any transformations fail" {
             val xa = List.of("1", "2", "x", "4")
             traverse(xa) { a ->
                 catches { a.toInt() }
@@ -51,13 +56,13 @@ class Exercise5 : WordSpec({
     }
 
     "sequence" should {
-        "!turn a list of some options into an option of list" {
+        "turn a list of some options into an option of list" {
             val lo =
                 List.of(Some(10), Some(20), Some(30))
             sequence(lo) shouldBe Some(List.of(10, 20, 30))
         }
 
-        "!turn a list of options containing a none into a none" {
+        "turn a list of options containing a none into a none" {
             val lo =
                 List.of(Some(10), None, Some(30))
             sequence(lo) shouldBe None
