0ad480faf69e66698efc9fc131831bda08998d6d
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-02-22 19:20:49 +0100

    Excercise 5.6

--- src/test/kotlin/chapter5/exercises/ex6/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex6/listing.kt
@@ -8,22 +8,21 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise6 : WordSpec({
 
     //tag::init[]
     fun <A> Stream<A>.headOption(): Option<A> =
+        foldRight({ Option.empty()}) {h,_ -> Some(h)}
 
-        SOLUTION_HERE()
     //end::init[]
 
     "Stream.headOption" should {
-        "!return some first element from the stream if it is not empty" {
+        "return some first element from the stream if it is not empty" {
             val s = Stream.of(1, 2, 3, 4)
             s.headOption() shouldBe Some(1)
         }
 
-        "!return none if the stream is empty" {
+        "return none if the stream is empty" {
             Stream.empty<Int>().headOption() shouldBe None
         }
     }
