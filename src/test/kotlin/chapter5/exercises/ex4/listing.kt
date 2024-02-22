7d785e4f14af31878311f7a5184960a36bacc095
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-02-22 19:10:57 +0100

    Excercise 5.4

--- src/test/kotlin/chapter5/exercises/ex4/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex4/listing.kt
@@ -1,25 +1,27 @@
 package chapter5.exercises.ex4
 
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
+import chapter5.solutions.ex4.forAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
 //tag::init[]
 fun <A> Stream<A>.forAll(p: (A) -> Boolean): Boolean =
+   foldRight({true}) {a,b -> p(a) && b() }
 
-    SOLUTION_HERE()
 //end::init[]
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise4 : WordSpec({
 
     "Stream.forAll" should {
-        "!ensure that all elements match the predicate" {
+        "ensure that all elements match the predicate" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.forAll { it < 6 } shouldBe true
         }
-        "!stop evaluating if one element does not satisfy the predicate" {
+        "stop evaluating if one element does not satisfy the predicate" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.forAll { it != 3 } shouldBe false
         }
