d7b612c0834d56e2ca5393622862836006c41aeb
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-02-23 18:36:22 +0100

    Excercise 5.10

--- src/test/kotlin/chapter5/exercises/ex10/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex10/listing.kt
@@ -1,24 +1,29 @@
 package chapter5.exercises.ex10
 
 import chapter3.List
+import chapter5.Empty
 import chapter5.Stream
-import chapter5.toList
+import chapter5.Stream.Companion.cons
+import chapter5.Stream.Companion.empty
 import chapter5.solutions.ex13.take
+import chapter5.solutions.ex7.append
+import chapter5.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise10 : WordSpec({
-
     //tag::init[]
-    fun fibs(): Stream<Int> =
+    fun fibs(): Stream<Int> {
+        fun go(a: Int, b: Int): Stream<Int> =
+            cons({ a }) { go(b, a + b) }
 
-        SOLUTION_HERE()
+        return go(0, 1)
+    }
+
     //end::init[]
 
     "fibs" should {
-        "!return a Stream of fibonacci sequence numbers" {
+        "return a Stream of fibonacci sequence numbers" {
             fibs().take(10).toList() shouldBe
                 List.of(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
         }
