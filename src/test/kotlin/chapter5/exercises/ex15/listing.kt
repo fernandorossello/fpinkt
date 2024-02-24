827308c167e91d051eed23bf9d2e65e003e1b412
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-02-24 11:49:20 +0100

    Excercise 5.15

--- src/test/kotlin/chapter5/exercises/ex15/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex15/listing.kt
@@ -1,29 +1,41 @@
 package chapter5.exercises.ex15
 
 import chapter3.List
+import chapter4.None
+import chapter4.Some
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
+import chapter5.Stream.Companion.unfold
 import chapter5.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 import chapter3.Cons as ConsL
 import chapter3.Nil as NilL
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise15 : WordSpec({
 
     //tag::tails[]
+    // fun <A, S> unfold(z: S, f: (S) -> Option<Pair<A, S>>): Stream<A> =
     fun <A> Stream<A>.tails(): Stream<Stream<A>> =
-
-        SOLUTION_HERE()
+        unfold(this) { s: Stream<A> ->
+            when(s) {
+                is Empty -> None
+                is Cons -> Some(s to s.tail())
+            }
+        }
     //end::tails[]
 
     fun <A, B> List<A>.map(f: (A) -> B): List<B> =
+        when(this) {
+            is NilL -> NilL
+            is ConsL -> ConsL(f(this.head), this.tail.map(f))
+        }
 
-        SOLUTION_HERE()
+
 
     "Stream.tails" should {
-        "!return the stream of suffixes of the input sequence" {
+        "return the stream of suffixes of the input sequence" {
             Stream.of(1, 2, 3)
                 .tails()
                 .toList()
@@ -31,8 +43,7 @@ class Exercise15 : WordSpec({
                 List.of(
                     ConsL(1, ConsL(2, ConsL(3, NilL))),
                     ConsL(2, ConsL(3, NilL)),
-                    ConsL(3, NilL),
-                    NilL
+                    ConsL(3, NilL)
                 )
         }
     }
