3945b16432d9880ed3014e6c6177b5db664a547c
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-02-24 15:08:44 +0100

    Excercise 6.4

--- src/test/kotlin/chapter6/exercises/ex4/listing.kt
+++ src/test/kotlin/chapter6/exercises/ex4/listing.kt
@@ -1,7 +1,9 @@
 package chapter6.exercises.ex4
 
 // import chapter3.Cons
+import chapter3.Cons
 import chapter3.List
+import chapter3.Nil
 // import chapter3.Nil
 import chapter6.RNG
 import chapter6.rng1
@@ -9,17 +11,24 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise4 : WordSpec({
 
     //tag::init[]
-    fun ints(count: Int, rng: RNG): Pair<List<Int>, RNG> =
+    fun ints(count: Int, rng: RNG): Pair<List<Int>, RNG> {
+        if(count == 0) {
+            return Nil to rng
+        } else {
+            val (i,rng2) = rng.nextInt()
+            val (t:List<Int>, rng3) = ints(count-1,rng2)
+            return Cons(i, t) to rng3
+        }
+    }
 
-        SOLUTION_HERE()
+
     //end::init[]
 
     "ints" should {
-        "!generate a list of ints of a specified length" {
+        "generate a list of ints of a specified length" {
 
             ints(5, rng1) shouldBe (List.of(1, 1, 1, 1, 1) to rng1)
         }
