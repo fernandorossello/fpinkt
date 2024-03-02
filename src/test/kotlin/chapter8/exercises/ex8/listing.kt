c91a0dd4e6a27a19947e7d45be125f87dd7fc72c
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-02 13:09:41 +0100

    Excercise 8.8

--- src/test/kotlin/chapter8/exercises/ex8/listing.kt
+++ src/test/kotlin/chapter8/exercises/ex8/listing.kt
@@ -2,6 +2,7 @@ package chapter8.exercises.ex8
 
 import chapter8.RNG
 import chapter8.State
+import chapter8.double
 import utils.SOLUTION_HERE
 
 data class Gen<A>(val sample: State<RNG, A>) {
@@ -12,8 +13,10 @@ data class Gen<A>(val sample: State<RNG, A>) {
             pga: Pair<Gen<A>, Double>,
             pgb: Pair<Gen<A>, Double>
         ): Gen<A> =
+            doubleGen().flatMap { d:Double -> (if(pga.second <= d) pga else pgb).first }
 
-            SOLUTION_HERE()
+        private fun doubleGen() :Gen<Double> = Gen(State { rng: RNG -> double(rng) })
+
         //end::init[]
     }
 
