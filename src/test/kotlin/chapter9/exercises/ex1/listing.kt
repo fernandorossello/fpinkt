7db7ef091b58b8fd3e164a0e916e510a3ca691b7
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-09 13:06:38 +0100

    Exercise 9.1

--- src/test/kotlin/chapter9/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter9/exercises/ex1/listing.kt
@@ -7,19 +7,23 @@ import utils.SOLUTION_HERE
 
 abstract class Listing : ParserDsl<ParseError>() {
 
+    /*
+    Using product, implement the now-familiar combinator map2.
+    In turn, use this to implement many1 in terms of many.
+     */
+
     //tag::init1[]
     override fun <A, B, C> map2(
         pa: Parser<A>,
         pb: () -> Parser<B>,
         f: (A, B) -> C
     ): Parser<C> =
-
-        SOLUTION_HERE()
+        (pa product pb).map { (a:A,b:B) -> f(a,b) }
     //end::init1[]
 
     //tag::init2[]
     override fun <A> many1(p: Parser<A>): Parser<List<A>> =
+        map2(p,{p.many()}) {a:A, b:List<A> -> listOf(a) + b}
 
-        SOLUTION_HERE()
     //end::init2[]
 }
