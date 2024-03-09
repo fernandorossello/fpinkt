1c3efe0386f88bef3ba2c98d924cec3177144583
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-09 13:19:06 +0100

    Exercise 9.3

--- src/test/kotlin/chapter9/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter9/exercises/ex3/listing.kt
@@ -7,17 +7,24 @@ import utils.SOLUTION_HERE
 
 abstract class Listing : ParserDsl<ParseError>() {
 
+    /*
+     Before continuing, see if you can define many in terms of or, map2, and succeed.
+     */
+
     fun <A, B, C> map2(
         pa: Parser<A>,
         pb: Parser<B>,
         f: (A, B) -> C
-    ): Parser<C> = TODO()
+    ): Parser<C> =
+        (pa product {pb}).map { (a:A,b:B) -> f(a,b) }
 
     init {
         //tag::init1[]
+        // zero or more
         fun <A> many(pa: Parser<A>): Parser<List<A>> =
+            map2(pa, many(pa)) {a: A,b:List<A> -> (listOf(a) + b) } or succeed(
+                emptyList())
 
-            SOLUTION_HERE()
         //end::init1[]
     }
 }
