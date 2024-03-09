6a86d2e947555b18e618ba67d23ce3a90e307099
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-09 17:37:18 +0100

    Exercise 9.8

--- src/test/kotlin/chapter9/exercises/ex8/listing.kt
+++ src/test/kotlin/chapter9/exercises/ex8/listing.kt
@@ -1,5 +1,6 @@
 package chapter9.exercises.ex8
 
+import chapter9.solutions.final.Example.flatMap
 import chapter9.solutions.final.ParseError
 import chapter9.solutions.final.Parser
 import chapter9.solutions.final.ParserDsl
@@ -10,8 +11,7 @@ abstract class Listing : ParserDsl<ParseError>() {
     init {
         //tag::init1[]
         fun <A, B> map(pa: Parser<A>, f: (A) -> B): Parser<B> =
-
-            SOLUTION_HERE()
+            pa.flatMap { succeed(f(it)) }
         //end::init1[]
     }
 }
