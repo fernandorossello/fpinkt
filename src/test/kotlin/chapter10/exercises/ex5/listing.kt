a58ed7e2cf89b79e3028733e935d71bc036e41f3
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-16 11:48:10 +0100

    Exercise 10.5

--- src/test/kotlin/chapter10/exercises/ex5/listing.kt
+++ src/test/kotlin/chapter10/exercises/ex5/listing.kt
@@ -1,10 +1,11 @@
 package chapter10.exercises.ex5
 
+import arrow.core.extensions.list.foldable.fold
+import arrow.core.extensions.list.foldable.foldLeft
 import chapter10.sec1.Monoid
 import utils.SOLUTION_HERE
 
 //tag::init1[]
 fun <A, B> foldMap(la: List<A>, m: Monoid<B>, f: (A) -> B): B =
-
-    SOLUTION_HERE()
+    la.foldLeft(m.nil) {b:B, a:A -> m.combine(b,f(a)) }
 //end::init1[]
