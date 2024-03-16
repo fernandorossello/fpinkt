a6a429824fbcd2b0bf99de06dfceefb5c1395c0e
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-16 12:09:13 +0100

    Exercise 10.7

--- src/test/kotlin/chapter10/exercises/ex7/listing.kt
+++ src/test/kotlin/chapter10/exercises/ex7/listing.kt
@@ -2,38 +2,50 @@ package chapter10.exercises.ex7
 
 import chapter10.Monoid
 import chapter10.stringMonoid
+import chapter2.exercises.ex2.head
+import chapter2.exercises.ex2.tail
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
 //tag::init1[]
-fun <A, B> foldMap(la: List<A>, m: Monoid<B>, f: (A) -> B): B =
+fun <A, B> foldMap(la: List<A>, m: Monoid<B>, f: (A) -> B): B {
+    if(la.isEmpty()) {
+        return m.nil
+    }
 
-    SOLUTION_HERE()
+    if (la.size == 1) {
+        return f(la.head)
+    }
+
+    return m.combine(
+        foldMap(la.take(la.size / 2), m, f),
+        foldMap(la.drop(la.size / 2), m, f)
+    )
+}
 //end::init1[]
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise7 : WordSpec({
     "balanced folding foldMap" should {
-        "!fold a list with an even number of values" {
+        "fold a list with an even number of values" {
             foldMap(
                 listOf("lorem", "ipsum", "dolor", "sit"),
                 stringMonoid
             ) { it } shouldBe "loremipsumdolorsit"
         }
-        "!fold a list with an odd number of values" {
+        "fold a list with an odd number of values" {
             foldMap(
                 listOf("lorem", "ipsum", "dolor"),
                 stringMonoid
             ) { it } shouldBe "loremipsumdolor"
         }
-        "!fold a list with a single value" {
+        "fold a list with a single value" {
             foldMap(
                 listOf("lorem"),
                 stringMonoid
             ) { it } shouldBe "lorem"
         }
-        "!fold an empty list" {
+        "fold an empty list" {
             foldMap(
                 emptyList<String>(),
                 stringMonoid
