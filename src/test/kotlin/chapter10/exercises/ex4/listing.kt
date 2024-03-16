c2da03abfaa527a85ab749a3e3682733023ccf89
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-16 10:44:20 +0100

    Exercise 10.4

--- src/test/kotlin/chapter10/exercises/ex4/listing.kt
+++ src/test/kotlin/chapter10/exercises/ex4/listing.kt
@@ -7,18 +7,24 @@ import chapter8.Passed
 import chapter8.SimpleRNG
 import chapter8.Gen
 import chapter8.Prop
+import chapter8.sec4_11.combine
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
 //tag::init1[]
 fun <A> monoidLaws(m: Monoid<A>, gen: Gen<A>): Prop =
-
-    SOLUTION_HERE()
+    Prop.Companion.forAll(
+        gen.flatMap { a1:A ->
+            gen.flatMap {a2:A ->
+                gen.map { a3:A -> Triple(a1,a2,a3) }
+            }
+        }) { a:Triple<A,A,A> ->
+        m.combine(a.first, m.combine(a.second,a.third)) == m.combine(m.combine(a.first,a.second),a.third)
+    }
 //end::init1[]
 
 //tag::init2[]
-//TODO: Enable tests by removing `!` prefix
 class Exercise4 : WordSpec({
     val max = 100
     val count = 100
@@ -26,7 +32,7 @@ class Exercise4 : WordSpec({
     val intGen = Gen.choose(-10000, 10000)
 
     "law of associativity" should {
-        "!be upheld using existing monoids" {
+        "be upheld using existing monoids" {
             monoidLaws(intAdditionMonoid, intGen)
                 .check(max, count, rng) shouldBe Passed
 
