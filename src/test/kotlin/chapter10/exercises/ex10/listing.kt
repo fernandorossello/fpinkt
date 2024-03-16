047803549c95dbb27a397325ea0f1c4747a8a7ae
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-16 12:40:12 +0100

    Exercise 10.10

--- src/test/kotlin/chapter10/exercises/ex10/listing.kt
+++ src/test/kotlin/chapter10/exercises/ex10/listing.kt
@@ -8,7 +8,21 @@ data class Stub(val chars: String) : WC()
 data class Part(val ls: String, val words: Int, val rs: String) : WC()
 
 //tag::init1[]
-fun wcMonoid(): Monoid<WC> =
+fun wcMonoid(): Monoid<WC> = object : Monoid<WC> {
+    override fun combine(a1: WC, a2: WC): WC =
+        when(a1) {
+            is Part -> when(a2) {
+                is Part -> Part(a1.ls, a1.words+a2.words+1,a2.rs)
+                is Stub -> Part(a1.ls, a1.words,a1.rs+a2.chars)
+            }
+            is Stub -> when(a2) {
+                is Part -> Part(a1.chars+a2.ls, a2.words,a2.rs)
+                is Stub -> Stub(a1.chars+a2.chars)
+            }
+        }
 
-    SOLUTION_HERE()
+    override val nil: WC
+        get() = Stub("")
+}
+
 //end::init1[]
