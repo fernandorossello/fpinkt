1c5bc3d88084e2ffc6cff5cd7ac330dac5afc12c
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-03-18 20:51:25 +0100

    Exercise 11.1

--- src/test/kotlin/chapter11/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter11/exercises/ex1/listing.kt
@@ -1,23 +1,44 @@
 package chapter11.exercises.ex1
 
+import arrow.Kind
 import arrow.core.ForListK
 import arrow.core.ForSequenceK
 import chapter10.ForList
 import chapter10.ForOption
+import chapter10.fix
 import chapter11.ForPar
+import chapter11.Par
+import chapter10.Some
+import chapter11.fix
 import chapter11.sec2.Monad
 import utils.SOLUTION_HERE
 
 //tag::init1[]
 object Monads {
 
-    fun parMonad(): Monad<ForPar> =
+    fun parMonad(): Monad<ForPar> = object : Monad<ForPar> {
+        override fun <A> unit(a: A): Kind<ForPar, A> =
+            Par.unit(a)
 
-        SOLUTION_HERE()
 
-    fun optionMonad(): Monad<ForOption> =
+        override fun <A, B> flatMap(
+            fa: Kind<ForPar, A>,
+            f: (A) -> Kind<ForPar, B>
+        ): Kind<ForPar, B> =
+            fa.fix().flatMap { f(it).fix() }
+    }
 
-        SOLUTION_HERE()
+
+    fun optionMonad(): Monad<ForOption> = object : Monad<ForOption> {
+        override fun <A> unit(a: A): Kind<ForOption, A> =
+            Some(a)
+
+        override fun <A, B> flatMap(
+            fa: Kind<ForOption, A>,
+            f: (A) -> Kind<ForOption, B>
+        ): Kind<ForOption, B> =
+            fa.fix().flatMap { f(it).fix() }
+    }
 
     fun listMonad(): Monad<ForList> =
 
