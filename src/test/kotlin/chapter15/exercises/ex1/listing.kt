1ea3031e7f57d591a426346789a753b03756d86b
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-04-01 19:58:20 +0200

    Exercise 15.1

--- src/test/kotlin/chapter15/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter15/exercises/ex1/listing.kt
@@ -1,11 +1,11 @@
 package chapter15.exercises.ex1
 
-// import chapter10.None
-// import chapter10.Option
-// import chapter10.Some
-// import chapter15.sec2.Await
-// import chapter15.sec2.Emit
-// import chapter15.sec2.Halt
+import chapter10.None
+import chapter10.Option
+import chapter10.Some
+import chapter15.sec2.Await
+import chapter15.sec2.Emit
+import chapter15.sec2.Halt
 import chapter15.sec2.Process
 import chapter15.sec2.toList
 import chapter3.List
@@ -16,44 +16,61 @@ import utils.SOLUTION_HERE
 
 //tag::init[]
 fun <I> take(n: Int): Process<I, I> =
-
-    SOLUTION_HERE()
+    Await { i: Option<I> ->
+        when (i) {
+            is None -> Halt<I, I>()
+            is Some -> if (n > 0) Emit(i.get, take(n - 1)) else Halt()
+        }
+    }.repeat()
 
 fun <I> drop(n: Int): Process<I, I> =
-
-    SOLUTION_HERE()
+    Await { i: Option<I> ->
+        when(i) {
+            is None -> Halt<I,I>()
+            is Some -> if(n>0) drop(n-1) else Emit<I,I>(i.get)
+        }
+    }.repeat()
 
 fun <I> takeWhile(p: (I) -> Boolean): Process<I, I> =
-
-    SOLUTION_HERE()
+    Await {i: Option<I> ->
+        when(i) {
+            is None -> Halt<I,I>()
+            is Some -> if(p(i.get)) Emit<I,I>(i.get, takeWhile(p)) else Halt()
+        }
+    }
 
 fun <I> dropWhile(p: (I) -> Boolean): Process<I, I> =
+    Await { i:Option<I> ->
+        when(i) {
+            is None -> Halt()
+            is Some -> if(p(i.get)) dropWhile(p) else Emit(i.get, dropWhile { false })
+        }
+    }
 
-    SOLUTION_HERE()
+
 //end::init[]
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise1 : WordSpec({
     val stream = Stream.of(1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1)
     "take" should {
-        "!consume the given number of elements from a stream" {
+        "consume the given number of elements from a stream" {
             take<Int>(5)(stream).toList() shouldBe List.of(1, 2, 3, 4, 5)
         }
     }
     "drop" should {
-        "!drop the given number of elements from a stream" {
+        "drop the given number of elements from a stream" {
             drop<Int>(5)(stream).toList() shouldBe
                 List.of(6, 7, 6, 5, 4, 3, 2, 1)
         }
     }
     "takeWhile" should {
-        "!consume elements from a stream while a predicate is true" {
+        "consume elements from a stream while a predicate is true" {
             takeWhile<Int> { 5 > it }(stream).toList() shouldBe
                 List.of(1, 2, 3, 4)
         }
     }
     "dropWhile" should {
-        "!drop elements from a stream while a predicate is true" {
+        "drop elements from a stream while a predicate is true" {
             dropWhile<Int> { 5 > it }(stream).toList() shouldBe
                 List.of(5, 6, 7, 6, 5, 4, 3, 2, 1)
         }
