fed82355c8647a09c7d9faa8c30e92699e71e71f
Fernando Rossello <fernando.rossello@glovoapp.com>
2024-04-01 21:00:36 +0200

    Exercise 15.2

--- src/test/kotlin/chapter15/exercises/ex2/listing.kt
+++ src/test/kotlin/chapter15/exercises/ex2/listing.kt
@@ -1,29 +1,35 @@
 package chapter15.exercises.ex2
 
-// import chapter10.None
-// import chapter10.Option
-// import chapter10.Some
-// import chapter15.sec2.Await
-// import chapter15.sec2.Emit
-// import chapter15.sec2.Halt
+ import chapter10.None
+ import chapter10.Option
+ import chapter10.Some
+ import chapter15.sec2.Await
+ import chapter15.sec2.Emit
+ import chapter15.sec2.Halt
 import chapter15.sec2.Process
 import chapter15.sec2.toList
 import chapter3.List
 import chapter5.Stream
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 //tag::init[]
-fun <I> count(): Process<I, Int> =
+fun <I> count(): Process<I, Int> {
+    fun go(acc: Int): Process<I,Int> =
+        Await { i: Option<I> ->
+            when (i) {
+                is None -> Halt<I,Int>()
+                is Some -> Emit(acc+1,go(acc+1))
+            }
+        }
+   return go(0)
+}
 
-    SOLUTION_HERE()
 //end::init[]
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise2 : WordSpec({
     "count" should {
-        "!emit a stream of numbers representing elements consumed" {
+        "emit a stream of numbers representing elements consumed" {
             val stream = Stream.of("a", "b", "c", "d")
             val p = count<String>()
             p(stream).toList() shouldBe List.of(1, 2, 3, 4)
